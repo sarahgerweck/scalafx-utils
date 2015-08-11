584da921ea7109ec9a51d8db68ada9ee11d63820
Sarah Gerweck <sarah.a180@gmail.com>
2015-08-11 11:34:20 -0700

    Fix the Sonatype staging location
    Publishing isn't possible with the one that comes from SBT for some
    reason.

--- project/Build.scala
+++ project/Build.scala
@@ -117,7 +117,7 @@ object Helpers {
 object Resolvers {
   val sonatypeSnaps   = Resolver.sonatypeRepo("snapshots")
   val sonatypeRelease = Resolver.sonatypeRepo("releases")
-  val sonatypeStaging = Resolver.sonatypeRepo("staging")
+  val sonatypeStaging = "Sonatype Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
 }
 
 object PublishSettings {
