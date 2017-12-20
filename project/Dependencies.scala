23ac4ed9899f3d3e8282415f28fa737c242810eb
Sarah Gerweck <sarah.a180@gmail.com>
2017-12-19 22:34:23 -0800

    Update Akka to 2.5.8

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
   /* ********************************************************************** */
   /*                                  Akka                                  */
   /* ********************************************************************** */
-  final val akkaVersion = "2.5.6"
+  final val akkaVersion = "2.5.8"
 
   val akkaActor  = "com.typesafe.akka" %% "akka-actor"  % akkaVersion
   val akkaAgent  = "com.typesafe.akka" %% "akka-agent"  % akkaVersion
